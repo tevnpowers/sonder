c4b088a2f366aeaf3bb485c1e002197858e4f505
Tev'n Powers <tevnpowers@gmail.com>
2024-02-03 09:23:04 -0500

    add barebones svelte scaffolding app with basic TipTap editor

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import TipTap from "$lib/components/TipTap.svelte";
+
+
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<TipTap/>

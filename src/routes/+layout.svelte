187b2e88a24ac71c296ddd1bd42e1a13f06ca456
Tev'n Powers <tevnpowers@gmail.com>
2024-02-04 11:36:29 -0500

    add logo to nav bar and change color

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,21 +1,23 @@
 <script>
+	import '../styles/fonts.css';
 	import '../app.pcss';
     import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from 'flowbite-svelte';
-    import TreeIcon from '$lib/images/tree.svelte'
+    import TreeIcon from '$lib/images/tree.svelte';
+	import logo from "$lib/images/sonderly_logo.png";
+
 </script>
 
 <div>
-	<Navbar let:NavContainer color="primary">
+	<Navbar let:NavContainer color="primary" class="bg-white">
         <NavContainer class="border px-5 py-2 rounded-lg bg-white dark:bg-gray-600">
           <NavBrand href="/">
-            <TreeIcon width={40} height={40}/>
+			<img src="{logo}" alt="Sonderly mascot"  width="60" height="60">
             <span class="self-center whitespace-nowrap text-xl font-semibold">Sonderly</span>
           </NavBrand>
           <NavHamburger />
           <NavUl>
             <NavLi href="/">Home</NavLi>
-            <NavLi href="/about">About</NavLi>
-            <NavLi href="/browse">Browse</NavLi>
+            <NavLi href="/post">Posts</NavLi>
             <NavLi href="/write">Write</NavLi>
           </NavUl>
         </NavContainer>
